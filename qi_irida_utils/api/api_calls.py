21e9f0f5d528d7c361658dfe5c3e11b89b6c3355
Andrea Telatin <andrea@telatin.com>
2019-04-15 15:49:20 +0100

    Removed YAML unsafe loader deprecation

--- qi_irida_utils/api/api_calls.py
+++ qi_irida_utils/api/api_calls.py
@@ -935,7 +935,7 @@ def initialize_api_from_config(config_path):
     """
     with open(config_path, 'r') as stream:
         try:
-            cfg = yaml.load(stream)
+            cfg = yaml.load(stream, Loader=yaml.FullLoader)
             client_id = cfg.get("client_id")
             client_secret = cfg.get("client_secret")
             base_url = cfg.get("base_url")
